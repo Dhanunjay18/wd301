c258bd97f643ce40057b33a64e6136dbfda49cdb
Dhanunjay18 <bhavanidhanunjay210@gmail.com>
2023-05-04 00:56:46 +0530

    L3-1

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import './TaskCard.css';
 interface TaskProp {
   title: string;
-  key: number;
   description: string;
   dueDate : string;
 }
-class Task extends React.Component<TaskProp> {
-  render() {
-    return (
-      <div className="TaskItem shadow-md border border-black border-slate-100">
-        <h2 className="text-base font-normal my-1">{this.props.title} ({this.props.dueDate})</h2>
-        <p className="text-sm text-slate-500">
-          {this.props.description}
-        </p>
-      </div>
-    );
-  }
-  }
 
-  export default Task;
+
+
+const Task = (props: TaskProp) => {
+  return (    
+    <div className="TaskItem">
+      <h2 className="text-base font-normal my-1">
+        {props.title} ({props.dueDate})</h2>
+      <p className="text-sm text-slate-500">
+        {props.description}
+      </p>      
+    </div>
+  );
+};
+
+export default Task;
--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 import Task from "./Task";
 import { TaskItem } from "./types";
 interface Props {
   tasks: TaskItem[];
 }
 interface State {}
-class TaskList extends React.Component<Props, State> {
+
+
+const TaskList = (props: Props) => {
+
+  const [tasks, setTasks] = useLocalStorage<Props>("tasks", {
+    tasks: [],
+  });
+
+  useEffect(() => {
+    console.log("Triggered");
+    localStorage.setItem('Tasks', JSON.stringify(tasks))
+  }, [tasks]);
+
+  const deleteTask = (idx: number) =>  {
+    console.log(idx);
+    const updatedTasks = [...props.tasks.slice(0, idx), ...props.tasks.slice(idx+1)]
+    setTasks({tasks: updatedTasks});
+    window.location.reload();
+  };
   
-  render() {
-    return this.props.tasks.map((task, idx) => (
-      <Task key={idx} title={task.title} dueDate={task.dueDate} description={task.description} />
-    ));
+  const list = () => {     
+    return props.tasks.map((task, idx) => (
+      <li>
+        <Task key={idx} title={task.title} dueDate={task.dueDate} description={task.description} />  
+        <button id="deleteTaskButton" className="deleteTaskButton" onClick={() => deleteTask(idx)}>Delete this Task {idx}</button>      
+      </li>
+      ));
+    }
+    return <>
+    <ol>
+      {list()}
+    </ol>
+    </>;
   }
-}
+
 export default TaskList;
