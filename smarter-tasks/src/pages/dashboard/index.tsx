9185fe99f4600e09d599a28e2e2f23130c7edada
Dhanunjay18 <bhavanidhanunjay210@gmail.com>
2023-07-03 19:46:47 +0530

    L6-2

--- smarter-tasks/src/pages/dashboard/index.tsx
+++ smarter-tasks/src/pages/dashboard/index.tsx
@@ -3,22 +3,26 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Dashboard: React.FC = () => {
-const userData = JSON.parse(localStorage.getItem('userData') || '{}');  
-const navigate = useNavigate();
-const handleSignout =async (event:React.FormEvent<HTMLFormElement>) => {
+  const navigate = useNavigate();
+  const handleSignout =async (event:React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    localStorage.removeItem('userData');
-    localStorage.removeItem('authToken');
+    try {
+      localStorage.removeItem('userData');
+      localStorage.removeItem('authToken');
+    } catch (error) {
+      console.log(error);
+    }
     navigate('/signin');
-}
+  }
+  const userData = JSON.parse(localStorage.getItem('userData') || '{}');  
   return (
     <div className="min-h-screen  items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Dashboard</h1>
-      <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Name : {userData.name}</h1>
-      <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Email : {userData.email} </h1>
-      <form onSubmit={handleSignout}>
-        <button id="logout-link"> Sign Out </button>
-      </form>
+      <div>
+        <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">Email : {userData.email} </h2>
+        <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">Name : {userData.name}</h2>
+      </div>
+      <button id="logout-link" onClick={handleSignout}> Sign Out </button>
     </div>
   );
 }
